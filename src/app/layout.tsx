70c8be8687aacaf587fa7c8208938fbc3fa78b70
Ivan Monichev <monichevid@baltech.ru>
2023-12-28 15:29:53 +0300

    feat: add assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,13 @@
 import { ReactNode } from 'react'
 import { Metadata } from 'next'
-import '@/styles/globals.css'
+import '@/assets/styles/style.css'
 
 export const metadata: Metadata = {
   title: 'Home',
   description: 'Welcome to Next.js'
 }
 
-const RootLayout = ({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children
-}: {
-  children: ReactNode
-}) => (
+const RootLayout = ({ children }: { children: ReactNode }) => (
   <html lang='en'>
     <body>{children}</body>
   </html>
